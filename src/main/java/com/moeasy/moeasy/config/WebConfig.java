e01a7d4d51f55aa1cf53a36fbcb613a3469ceced
JaeYoung-Cho-95 <whwo9745@gmail.com>
2025-08-03 19:39:16 +0900

    cors error fix

--- src/main/java/com/moeasy/moeasy/config/WebConfig.java
+++ src/main/java/com/moeasy/moeasy/config/WebConfig.java
@@ -9,10 +9,15 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("*") // 모든 출처에서 오는 요청을 허용
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 HTTP 메서드
-                .allowedHeaders("*") // 모든 헤더를 허용
-                .allowCredentials(true); // 인증정보 허용
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",             // 로컬 (React, Vite 등에서 기본 포트)
+                        "http://127.0.0.1:3000",             // 혹시 IP로 접속할 경우
+                        "https://mo-easy.com",
+                        "https://api.mo-easy.com"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
