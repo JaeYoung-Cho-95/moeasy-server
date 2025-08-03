195ffbf99360f4d32fb3b5a9fc8020ce8d03d73c
JaeYoung-Cho-95 <whwo9745@gmail.com>
2025-08-03 15:16:16 +0900

    first commit

---
+++ src/main/java/com/moeasy/moeasy/MoeasyApplication.java
@@ -0,0 +1,13 @@
+package com.moeasy.moeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoeasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoeasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moeasy/moeasy/MoeasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moeasy.moeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoeasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
