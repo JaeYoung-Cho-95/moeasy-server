b317f41f3ce5ca4b687ebd16709f64dab7587579
JaeYoung-Cho-95 <whwo9745@gmail.com>
2025-08-03 17:06:21 +0900

    timezone korea 지정

--- src/main/java/com/moeasy/moeasy/MoeasyApplication.java
+++ src/main/java/com/moeasy/moeasy/MoeasyApplication.java
@@ -1,10 +1,18 @@
 package com.moeasy.moeasy;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class MoeasyApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoeasyApplication.class, args);
