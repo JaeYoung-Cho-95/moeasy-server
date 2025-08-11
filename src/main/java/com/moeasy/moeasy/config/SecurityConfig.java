7fe30ca83b1e4398e429864f0e1ed5c67132f17b
JaeYoung-Cho-95 <whwo9745@gmail.com>
2025-08-11 23:50:21 +0900

    add survey

--- src/main/java/com/moeasy/moeasy/config/SecurityConfig.java
+++ src/main/java/com/moeasy/moeasy/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         // 로그인, 회원가입 등 인증이 필요 없는 경로는 permitAll()로 설정
                         .requestMatchers(
-                                "/suvery",
+                                "/survey",
                                 "/account/test/token",
                                 "/account/login",
                                 "/questions/verifyQrCode",
