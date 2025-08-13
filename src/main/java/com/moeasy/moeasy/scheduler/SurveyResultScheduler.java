712960e354b2b64e70ae754f4e9a27455711a71a
JaeYoung-Cho-95 <whwo9745@gmail.com>
2025-08-13 12:40:16 +0900

    keyworrds 조회 시 null 값 처리

--- src/main/java/com/moeasy/moeasy/scheduler/SurveyResultScheduler.java
+++ src/main/java/com/moeasy/moeasy/scheduler/SurveyResultScheduler.java
@@ -6,125 +6,142 @@ import com.moeasy.moeasy.domain.question.Question;
 import com.moeasy.moeasy.domain.survey.Survey;
 import com.moeasy.moeasy.repository.question.QuestionRepository;
 import com.moeasy.moeasy.repository.survey.SurveyRepository;
-import com.moeasy.moeasy.scheduler.dto.*;
+import com.moeasy.moeasy.scheduler.dto.ContentDto;
+import com.moeasy.moeasy.scheduler.dto.GraphItemDto;
+import com.moeasy.moeasy.scheduler.dto.KeywordsItemsDto;
+import com.moeasy.moeasy.scheduler.dto.KeywordsResponseDto;
+import com.moeasy.moeasy.scheduler.dto.SaveDataDto;
+import com.moeasy.moeasy.scheduler.dto.SentencesListDto;
 import com.moeasy.moeasy.scheduler.util.CleanJson;
 import com.moeasy.moeasy.scheduler.util.Pick20InShortQuestions;
 import com.moeasy.moeasy.scheduler.util.SurveyUtils;
 import com.moeasy.moeasy.service.llm.NaverCloudStudioService;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.List;
-
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class SurveyResultScheduler {
-    private static final ZoneId ZONE_ID = ZoneId.of("Asia/Seoul");
-    private final SurveyRepository surveyRepository;
-    private final QuestionRepository questionRepository;
-    private final NaverCloudStudioService naverCloudStudioService;
 
-    @Transactional
-    @Scheduled(cron = "0 */10 * * * *", zone = "Asia/Seoul")
-    public void logSurveyResultsUpdatedInLast10Min() {
-        LocalDateTime now = LocalDateTime.now(ZONE_ID);
-        LocalDateTime tenMinutesAgo = now.minusMinutes(10);
+  private static final ZoneId ZONE_ID = ZoneId.of("Asia/Seoul");
+  private final SurveyRepository surveyRepository;
+  private final QuestionRepository questionRepository;
+  private final NaverCloudStudioService naverCloudStudioService;
 
-        List<Survey> updated = surveyRepository
-                .findAllByLastUpdatedGreaterThanEqualAndLastUpdatedLessThan(tenMinutesAgo, now);
+  @Transactional
+  @Scheduled(cron = "0 */10 * * * *", zone = "Asia/Seoul")
+  public void logSurveyResultsUpdatedInLast10Min() {
+    LocalDateTime now = LocalDateTime.now(ZONE_ID);
+    LocalDateTime tenMinutesAgo = now.minusMinutes(10);
 
-        if (updated.isEmpty()) {
-            log.info("[SurveyResults] 최근 10분({} ~ {}) 동안 갱신된 레코드가 없습니다.", tenMinutesAgo, now);
-            return;
+    List<Survey> updated = surveyRepository
+        .findAllByLastUpdatedGreaterThanEqualAndLastUpdatedLessThan(tenMinutesAgo, now);
+
+    if (updated.isEmpty()) {
+      log.info("[SurveyResults] 최근 10분({} ~ {}) 동안 갱신된 레코드가 없습니다.", tenMinutesAgo, now);
+      return;
+    }
+
+    log.info("[SurveyResults] 최근 10분({} ~ {}) 갱신건수: {}", tenMinutesAgo, now, updated.size());
+
+    try {
+      for (Survey survey : updated) {
+        Question question = questionRepository.findBySurveyId(survey.getId());
+        Integer totalCount = question.getCount();
+        String json = survey.getResultsJson();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String summarizedJson = SurveyUtils.summarizeMaxChoices(json, objectMapper);
+        String summarizedJsonWithoutDemo = SurveyUtils.excludeAgeAndGender(summarizedJson,
+            objectMapper);
+        ContentDto contentDto = getContentData(summarizedJsonWithoutDemo, objectMapper);
+
+        List<GraphItemDto> graphData = SurveyUtils.extractGraphsData(json, objectMapper);
+        List<String> keywordsData = getKeywordsData(summarizedJson, objectMapper);
+        SentencesListDto sentencesData = getSentencesData(summarizedJson, totalCount, objectMapper);
+
+        SaveDataDto saveDataDto = SaveDataDto.builder()
+            .subject(question.getTitle())
+            .graphs(graphData)
+            .keywords(keywordsData)
+            .content(contentDto.getContent())
+            .sentences(sentencesData.getSentences())
+            .totalCount(totalCount)
+            .build();
+
+        String saveJson = objectMapper.writeValueAsString(saveDataDto); // 단일 라인 JSON
+        survey.updateSummarizeJson(saveJson);
+      }
+    } catch (Exception e) {
+      log.info(e.getMessage());
+    }
+  }
+
+  private ContentDto getContentData(String summarizedJson, ObjectMapper objectMapper)
+      throws IOException {
+    return getContentResponse(
+        naverCloudStudioService.getPromptWithFilePath("prompts/makeContent.txt"),
+        Pick20InShortQuestions.extract(summarizedJson, objectMapper)
+    );
+  }
+
+  private SentencesListDto getSentencesData(String summarizedJson, Integer totalCount,
+      ObjectMapper objectMapper) throws IOException {
+    return getSentencesListResponse(
+        naverCloudStudioService.getPromptWithFilePath("prompts/makeSentences.txt"),
+        CleanJson.toObjectiveWithPercent(summarizedJson, totalCount, objectMapper)
+    );
+  }
+
+  private List<String> getKeywordsData(String summarizedJson, ObjectMapper objectMapper)
+      throws IOException {
+    KeywordsResponseDto keywordsResponse = getKeywordsResponse(
+        naverCloudStudioService.getPromptWithFilePath("prompts/makeKeywordsUsingSurvey.txt"),
+        Pick20InShortQuestions.extract(summarizedJson, objectMapper)
+    );
+
+    List<String> keywordsData = new ArrayList<>();
+    if (keywordsResponse.getPositives() != null) {
+      for (KeywordsItemsDto keywordsItemsDto : keywordsResponse.getPositives()) {
+        keywordsData.add(keywordsItemsDto.getHashtag());
+      }
+    }
+
+    if (keywordsResponse.getNegatives() != null) {
+      for (KeywordsItemsDto keywordsItemsDto : keywordsResponse.getNegatives()) {
+        keywordsData.add(keywordsItemsDto.getHashtag());
+      }
+    }
+    return keywordsData;
+  }
+
+  private KeywordsResponseDto getKeywordsResponse(String systemPrompt, String inputPrompt) {
+    return naverCloudStudioService.chatHcx007(
+        systemPrompt, inputPrompt, new TypeReference<KeywordsResponseDto>() {
         }
+    );
+  }
 
-        log.info("[SurveyResults] 최근 10분({} ~ {}) 갱신건수: {}", tenMinutesAgo, now, updated.size());
-
-        try {
-            for (Survey survey : updated) {
-                Question question = questionRepository.findBySurveyId(survey.getId());
-                Integer totalCount = question.getCount();
-                String json = survey.getResultsJson();
-
-                ObjectMapper objectMapper = new ObjectMapper();
-                String summarizedJson = SurveyUtils.summarizeMaxChoices(json, objectMapper);
-                String summarizedJsonWithoutDemo = SurveyUtils.excludeAgeAndGender(summarizedJson, objectMapper);
-                ContentDto contentDto = getContentData(summarizedJsonWithoutDemo, objectMapper);
-
-                List<GraphItemDto> graphData = SurveyUtils.extractGraphsData(json, objectMapper);
-                List<String> keywordsData = getKeywordsData(summarizedJson, objectMapper);
-                SentencesListDto sentencesData = getSentencesData(summarizedJson, totalCount, objectMapper);
-
-                SaveDataDto saveDataDto = SaveDataDto.builder()
-                        .subject(question.getTitle())
-                        .graphs(graphData)
-                        .keywords(keywordsData)
-                        .content(contentDto.getContent())
-                        .sentences(sentencesData.getSentences())
-                        .totalCount(totalCount)
-                        .build();
-
-                String saveJson = objectMapper.writeValueAsString(saveDataDto); // 단일 라인 JSON
-                survey.updateSummarizeJson(saveJson);
-            }
-        } catch (Exception e) {
-            log.info(e.getMessage());
+  private SentencesListDto getSentencesListResponse(String systemPrompt, String inputPrompt) {
+    return naverCloudStudioService.chatHcx007(
+        systemPrompt, inputPrompt, new TypeReference<SentencesListDto>() {
         }
-    }
+    );
+  }
 
-    private ContentDto getContentData(String summarizedJson, ObjectMapper objectMapper) throws IOException {
-        return getContentResponse(
-                naverCloudStudioService.getPromptWithFilePath("prompts/makeContent.txt"),
-                Pick20InShortQuestions.extract(summarizedJson, objectMapper)
-        );
-    }
-
-    private SentencesListDto getSentencesData(String summarizedJson, Integer totalCount, ObjectMapper objectMapper) throws IOException {
-        return getSentencesListResponse(
-                naverCloudStudioService.getPromptWithFilePath("prompts/makeSentences.txt"),
-                CleanJson.toObjectiveWithPercent(summarizedJson, totalCount, objectMapper)
-        );
-    }
-
-    private List<String> getKeywordsData(String summarizedJson, ObjectMapper objectMapper) throws IOException {
-        KeywordsResponseDto keywordsResponse = getKeywordsResponse(
-                naverCloudStudioService.getPromptWithFilePath("prompts/makeKeywordsUsingSurvey.txt"),
-                Pick20InShortQuestions.extract(summarizedJson, objectMapper)
-        );
-
-        List<String> keywordsData = new ArrayList<>();
-        for (KeywordsItemsDto keywordsItemsDto : keywordsResponse.getPositives()) {
-            keywordsData.add(keywordsItemsDto.getHashtag());
+  private ContentDto getContentResponse(String systemPrompt, String inputPrompt) {
+    return naverCloudStudioService.chatHcx007(
+        systemPrompt, inputPrompt, new TypeReference<ContentDto>() {
         }
-        for (KeywordsItemsDto keywordsItemsDto : keywordsResponse.getNegatives()) {
-            keywordsData.add(keywordsItemsDto.getHashtag());
-        }
-        return keywordsData;
-    }
-
-    private KeywordsResponseDto getKeywordsResponse(String systemPrompt, String inputPrompt) {
-        return naverCloudStudioService.chatHcx007(
-                systemPrompt, inputPrompt, new TypeReference<KeywordsResponseDto>() {}
-        );
-    }
-
-    private SentencesListDto getSentencesListResponse(String systemPrompt, String inputPrompt) {
-        return naverCloudStudioService.chatHcx007(
-                systemPrompt, inputPrompt, new TypeReference<SentencesListDto>() {}
-        );
-    }
-
-    private ContentDto getContentResponse(String systemPrompt, String inputPrompt) {
-        return naverCloudStudioService.chatHcx007(
-                systemPrompt, inputPrompt, new TypeReference<ContentDto>() {}
-        );
-    }
+    );
+  }
 }
