40b5e7143147931978890b6aa9317f93553516c0
JaeYoung-Cho-95 <whwo9745@gmail.com>
2025-09-05 14:53:23 +0900

    refactor: remove unnecessary error response methods

--- src/main/java/com/moeasy/moeasy/config/response/GlobalExceptionHandler.java
+++ src/main/java/com/moeasy/moeasy/config/response/GlobalExceptionHandler.java
@@ -5,7 +5,6 @@ import com.moeasy.moeasy.config.response.responseDto.ErrorResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -29,14 +28,5 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     log.error("CustomFailException 발생: {}", e.getMessage(), e);
     ErrorResponseDto body = ErrorResponseDto.from(e.getCode(), e.getMessage());
     return ResponseEntity.status(e.getCode()).body(body);
-  }
-
-  @ExceptionHandler(HttpMessageNotReadableException.class)
-  public ResponseEntity<ErrorResponseDto> handleHttpMessageNotReadableException(
-      HttpMessageNotReadableException e) {
-    log.warn("잘못된 형식의 JSON 요청: {}", e.getMessage(), e);
-    ErrorResponseDto body = ErrorResponseDto.from(HttpStatus.BAD_REQUEST.value(),
-        "요청 형식이 올바르지 않습니다.");
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
   }
 }
